490d6e5bf273ff20fef87de22c56fdb63c60c2d1
yungbrave <SergeyIlinR@gmail.com>
2023-06-28 17:55:05 +0300

    deleted excess imports

--- src/main/java/web/config/JpaConfig.java
+++ src/main/java/web/config/JpaConfig.java
@@ -17,7 +17,6 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -12,8 +12,6 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
-import javax.servlet.FilterRegistration;
-
 
 @Configuration
 @EnableWebMvc
